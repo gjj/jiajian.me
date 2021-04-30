aea87a9b8d452e1b8990dec239b13c1ff660d087
Goi Jia Jian <jiajiannn@gmail.com>
2021-04-30 15:55:52 +0800

    feat: homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,43 @@
 import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import DisplayPicture from "../images/dp.jpg";
 
 function Home() {
   return (
-    <body className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover">
-      <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
-        <div
-          id="profile"
-          className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
-        >
-          <div className="p-4 md:p-12 text-center lg:text-left">
-            <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" />
-
-            <h1 className="text-3xl font-bold pt-8 lg:pt-0">Your Name</h1>
-            <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25" />
-            <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-              <svg
-                className="h-4 fill-current text-green-700 pr-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
+    <div className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover">
+      <div className="flex flex-col md:flex-row max-w-4xl items-center h-auto lg:h-screen mx-auto my-32 lg:my-0 justify-center">
+        <img
+          className="rounded-full shadow-xl mx-auto h-48 w-48 mb-4 lg:mb-0"
+          src={DisplayPicture}
+        />
+        <div className="w-full mx-6 lg:mx-0">
+          <div className="p-4 lg:p-12 text-center justify-center lg:text-left">
+            <p className="hidden lg:block">Hey! I’m</p>
+            <h1 className="text-4xl font-bold mt-2">Jia Jian</h1>
+            <div className="flex flex-row lg:justify-start justify-center gap-4 pt-4 lg:pt-2">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="LinkedIn"
+                title="LinkedIn"
+                href="https://www.linkedin.com/in/jiajian"
               >
-                <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
-              </svg>{" "}
-              What you do
-            </p>
+                <FaLinkedin className="text-3xl" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="GitHub"
+                title="GitHub"
+                href="https://github.com/gjj"
+              >
+                <FaGithub className="text-3xl" />
+              </a>
+            </div>
           </div>
         </div>
-
-        <div className="w-full lg:w-2/5">
-          <img
-            src="https://source.unsplash.com/MP0IUfwrn0A"
-            className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
-          />
-        </div>
       </div>
-    </body>
+    </div>
   );
 }
 
